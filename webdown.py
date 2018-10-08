899fb3493a7da4ce7517df47b23db393c44cad74
Yusuf Atmaca <42784726+rooturk@users.noreply.github.com>
2018-10-08 16:05:58 +0300

    Update webdown.py

--- webdown.py
+++ webdown.py
@@ -1,14 +1,5 @@
-# RooTurk Technology
+# RooTurk Software
 # www.RooTurk.com
-# ----------------------------------------------------------------------------------------------
-# webdos - HTTP Unbearable Load King
-#
-# this tool is a dos tool that is meant to put heavy load on HTTP servers in order to bring them
-# to their knees by exhausting the resource pool, its is meant for research purposes only
-# and any malicious usage of this tool is prohibited.
-#
-# author :  Yusuf ATMACA
-# ----------------------------------------------------------------------------------------------
 import urllib2
 import sys
 import threading
